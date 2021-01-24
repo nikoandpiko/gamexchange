93430b4f5229cd7a7d295fe9467a510e9c0f7c4c
David Smith <smitty6000@gmail.com>
2021-01-24 19:47:50 +0900

    fixed xbox seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require 'net/https'
 http1 = Net::HTTP.new('api.igdb.com', 443)
 http1.use_ssl = true
 request1 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request1.body = 'fields artworks,cover,category,genres.name,name,rating, platforms; where platforms = 48 & rating > 90; limit 100;'
+request1.body = 'fields artworks,cover,category,genres.name,name,rating, platforms; where platforms = 48  & rating > 70; limit 100;'
 
 hash_as_string1 = http1.request(request1).body
 playstation_games_data_array = eval(hash_as_string1)
@@ -20,19 +20,19 @@ playstation_games_data_array = eval(hash_as_string1)
 http2 = Net::HTTP.new('api.igdb.com', 443)
 http2.use_ssl = true
 request2 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request2.body = 'fields artworks,cover,category,genres.name,name,rating, platforms; where platforms = 130 & rating > 90; limit 100;'
+request2.body = 'fields artworks,cover,category,genres.name,name,rating, platforms; where platforms = 130 & rating > 70; limit 100;'
 
 hash_as_string2 = http2.request(request2).body
 switch_games_data_array = eval(hash_as_string2)
 
 
-# http3 = Net::HTTP.new('api.igdb.com', 443)
-# http3.use_ssl = true
-# request3 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-# request3.body = 'fields artworks,cover,category,genres.name,name,rating, platforms; where platforms = 49; limit 100;'
+http3 = Net::HTTP.new('api.igdb.com', 443)
+http3.use_ssl = true
+request3 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
+request3.body = 'fields artworks,cover,category,genres.name,name,rating, platforms; where platforms = 49; limit 100;'
 
-# hash_as_string3 = http3.request(request3).body
-# xbox_games_data_array = eval(hash_as_string3)
+hash_as_string3 = http3.request(request3).body
+xbox_games_data_array = eval(hash_as_string3)
 
 
 puts "Cleaning up database"
@@ -44,7 +44,8 @@ puts "Seeding playstation games"
 300.times do 
     playstation_game = playstation_games_data_array.sample
     playstation_game_name = playstation_game[:name]
-    playstation_game_genre = playstation_game[:genres][0][:name]
+    playstation_game[:genres].nil? ? playstation_game_genre = "" : playstation_game_genre = playstation_game[:genres][0][:name]
+
     Game.create(
         title: playstation_game_name,
         platform: "Playstation 4",
@@ -60,7 +61,8 @@ puts "Seeding nintendo games"
 300.times do 
     switch_game = switch_games_data_array.sample
     switch_game_name = switch_game[:name]
-    switch_game_genre = switch_game[:genres][0][:name]
+    switch_game[:genres].nil? ? switch_game_genre = "" : switch_game_genre = switch_game[:genres][0][:name]
+
     Game.create(
         title: switch_game_name,
         platform: "Nintendo Switch",
@@ -70,16 +72,17 @@ end
 puts "Seeded"
 
 
-# puts "Seeding xbox one games"
+puts "Seeding xbox one games"
 
-# 300.times do 
-#     xbox_game = xbox_games_data_array.sample
-#     xbox_game_name = xbox_game[:name]
-#     xbox_game_genre = xbox_game[:genres][0][:name]
-#     Game.create(
-#         title: xbox_game_name,
-#         platform: "Xbox One",
-#         genre: xbox_game_genre
-# )
-# end
-# puts "Seeded"
+300.times do 
+    xbox_game = xbox_games_data_array.sample
+    xbox_game_name = xbox_game[:name]
+    xbox_game[:genres].nil? ? xbox_game_genre = "" : xbox_game_genre = xbox_game[:genres][0][:name]
+
+    Game.create(
+        title: xbox_game_name,
+        platform: "Xbox One",
+        genre: xbox_game_genre
+)
+end
+puts "Seeded"

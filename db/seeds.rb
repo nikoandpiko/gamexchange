d7eb323de06fb0871932c2eab4daeae27473009d
David Smith <smitty6000@gmail.com>
2021-01-27 00:26:45 +0900

    added image url

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require 'net/https'
 http1 = Net::HTTP.new('api.igdb.com', 443)
 http1.use_ssl = true
 request1 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request1.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 48  & rating > 70; limit 1;'
+request1.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 48  & rating > 70; limit 75;'
 
 hash_as_string1 = http1.request(request1).body
 playstation_games_data_array = eval(hash_as_string1)
@@ -20,7 +20,7 @@ playstation_games_data_array = eval(hash_as_string1)
 http2 = Net::HTTP.new('api.igdb.com', 443)
 http2.use_ssl = true
 request2 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request2.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 130 & rating > 70; limit 100;'
+request2.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 130 & rating > 70; limit 75;'
 
 hash_as_string2 = http2.request(request2).body
 switch_games_data_array = eval(hash_as_string2)
@@ -29,7 +29,7 @@ switch_games_data_array = eval(hash_as_string2)
 http3 = Net::HTTP.new('api.igdb.com', 443)
 http3.use_ssl = true
 request3 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request3.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 49; limit 10;'
+request3.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 49; limit 25;'
 
 hash_as_string3 = http3.request(request3).body
 xbox_games_data_array = eval(hash_as_string3)
@@ -48,7 +48,7 @@ puts "All clean"
 
 puts "Seeding playstation games"
 
-2.times do 
+100.times do 
     playstation_game = playstation_games_data_array.sample
     playstation_game_name = playstation_game[:name]
     playstation_game[:genres].nil? ? playstation_game_genre = "" : playstation_game_genre = playstation_game[:genres][0][:name]
@@ -76,7 +76,7 @@ puts "Seeded"
 
 puts "Seeding nintendo games"
 
-300.times do 
+100.times do 
     switch_game = switch_games_data_array.sample
     switch_game_name = switch_game[:name]
     switch_game[:genres].nil? ? switch_game_genre = "" : switch_game_genre = switch_game[:genres][0][:name]
@@ -98,7 +98,7 @@ puts "Seeded"
 
 puts "Seeding xbox one games"
 
-300.times do 
+50.times do 
     xbox_game = xbox_games_data_array.sample
     xbox_game_name = xbox_game[:name]
     xbox_game[:genres].nil? ? xbox_game_genre = "" : xbox_game_genre = xbox_game[:genres][0][:name]

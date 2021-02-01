b8fb211a09f90ffc62cf99a54e9d80381aec8bb5
David Smith <smitty6000@gmail.com>
2021-02-01 23:11:31 +0900

    seed now pulls a platforms array for each game

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,32 +19,10 @@ require 'net/https'
 http1 = Net::HTTP.new('api.igdb.com', 443)
 http1.use_ssl = true
 request1 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request1.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 48  & rating > 70; limit 75;'
+request1.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = (48,49,130) & rating > 80; limit 500;'
 
 hash_as_string1 = http1.request(request1).body
-playstation_games_data_array = eval(hash_as_string1)
-
-
-http2 = Net::HTTP.new('api.igdb.com', 443)
-http2.use_ssl = true
-request2 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request2.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 130 & rating > 70; limit 75;'
-
-hash_as_string2 = http2.request(request2).body
-switch_games_data_array = eval(hash_as_string2)
-
-
-http3 = Net::HTTP.new('api.igdb.com', 443)
-http3.use_ssl = true
-request3 = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => '9nu16mxtee9islxz909mkmudl8minu', 'Authorization' => 'Bearer sgbs6fxnj0thm1op0dzog9n5if0ygx'})
-request3.body = 'fields artworks,cover.*,category,genres.name,name,rating, platforms; where platforms = 49; limit 75;'
-
-hash_as_string3 = http3.request(request3).body
-xbox_games_data_array = eval(hash_as_string3)
-
-
-
-
+games_data_array = eval(hash_as_string1)
 
 
 
@@ -57,77 +35,38 @@ User.destroy_all
 Game.destroy_all
 puts "All clean"
 
-puts "Seeding playstation games"
+puts "Seeding games"
 
 
-100.times do
-    playstation_game = playstation_games_data_array.sample
-    playstation_game_name = playstation_game[:name]
-    if !playstation_game[:genres].nil? 
-    playstation_game_genre = playstation_game[:genres][0][:name]
-    playstation_game_image_small = playstation_game[:cover][:url]
+games_data_array.each do |game|
+    if game[:cover].nil? || game[:platforms].nil? || game[:genres].nil? || game[:name].nil? || game[:cover][:url].nil?
+        puts "passed"
+    else
+        puts "saved"
+        game_genre = game[:genres][0][:name]
+        game_image_small = game[:cover][:url]
+        game_platforms = game[:platforms]
+        game_name = game[:name]
 
-    playstation_game_image_small = playstation_game_image_small.split("/")
-    playstation_game_image_small[6] = "t_cover_big"
-    playstation_game_image = playstation_game_image_small.join("/")
+        game_image_small = game_image_small.split("/")
+        game_image_small[6] = "t_cover_big"
+        game_image = game_image_small.join("/")
+        
 
 
-    Game.create(
-        title: playstation_game_name,
-        genre: playstation_game_genre,
-        image: playstation_game_image
-    )
+        Game.create(
+            title: game_name,
+            genre: game_genre,
+            image: game_image,
+            platforms: game_platforms
+        )
     end
 end
+
 
 puts "Seeded"
 
 
-
-puts "Seeding nintendo games"
-
-100.times do
-    switch_game = switch_games_data_array.sample
-    switch_game_name = switch_game[:name]
-    if !switch_game[:genres].nil?
-    switch_game_genre = switch_game[:genres][0][:name]
-    switch_game_image_small = switch_game[:cover][:url]
-    switch_game_image_small = switch_game_image_small.split("/")
-    switch_game_image_small[6] = "t_cover_big"
-    switch_game_image = switch_game_image_small.join("/")
-
-    Game.create(
-        title: switch_game_name,
-        genre: switch_game_genre,
-        image: switch_game_image
-    )
-    end
-end
-puts "Seeded"
-
-
-puts "Seeding xbox one games"
-
-50.times do
-    xbox_game = xbox_games_data_array.sample
-    xbox_game_name = xbox_game[:name]
-    if !xbox_game[:genres].nil?
-    xbox_game_genre = xbox_game[:genres][0][:name]
-    xbox_game[:cover][:url].nil? ? xbox_game_image_small = "" : xbox_game_image_small = xbox_game[:cover][:url]
-
-    xbox_game_image_small = xbox_game_image_small.split("/")
-    xbox_game_image_small[6] = "t_cover_big"
-    xbox_game_image = xbox_game_image_small.join("/")
-
-
-    Game.create(
-        title: xbox_game_name,
-        genre: xbox_game_genre,
-        image: xbox_game_image
-    )
-    end
-end
-puts "Seeded"
 
 
 puts "Creating users"
@@ -220,6 +159,5 @@ end
     Offer.create(
     user_id: user_ids_for_seed.sample,
     game_id: game_ids_for_seed.sample,
-    platform: PLATFORMS.sample 
 )
 end

1f2ed51a17099fb85c6ffe95165408b64aef530a
Lawrence Kroll <larry43.lk@gmail.com>
2021-02-02 21:55:56 +0900

    user show updated with rental owner decision. updated buttons and policy

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,5 +2,5 @@ class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :offer
   validates :status, presence: true
-  STATUSES = ['Accepted', 'Declined', 'Pending', 'Returned', 'Archived']
+  STATUSES = ['Accepted', 'Declined', 'Pending', 'Returned', 'Archived', 'Request']
 end

ba66d2110698281041ad5c9c1e15dfce8dae3609
David Smith <smitty6000@gmail.com>
2021-02-06 12:09:17 +0900

    api pulls screenshots, screenshot display in offer show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_100055) do
+ActiveRecord::Schema.define(version: 2021_02_06_021949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_100055) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "platforms", default: [], array: true
+    t.string "screenshot"
   end
 
   create_table "offers", force: :cascade do |t|
